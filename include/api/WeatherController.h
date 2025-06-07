ef46b24675a5e2b75b15d88c40714d94fd32849a
Jason Schaufele <github@jasonschaufele.com>
2025-06-07 20:05:17 +0200

    Update weather controller to return live data as JSON

--- include/api/WeatherController.h
+++ include/api/WeatherController.h
@@ -5,12 +5,13 @@
 #include "oatpp/web/server/HttpConnectionHandler.hpp"
 #include "oatpp/web/server/HttpRouter.hpp"
 #include "oatpp/web/mime/ContentMappers.hpp"
-
 #include "oatpp/network/tcp/server/ConnectionProvider.hpp"
-
 #include "oatpp/json/ObjectMapper.hpp"
-
 #include "oatpp/macro/component.hpp"
+
+#include "WeatherClient.h"
+#include <nlohmann/json.hpp>
+#include <string>
 
 #include OATPP_CODEGEN_BEGIN(ApiController)
 
@@ -19,17 +20,40 @@ class WeatherController : public oatpp::web::server::api::ApiController {
     WeatherController(OATPP_COMPONENT(std::shared_ptr<oatpp::web::mime::ContentMappers>, contentMappers))
   		: oatpp::web::server::api::ApiController(contentMappers) {}
 
-
-
     ENDPOINT("GET", "/weather", getWeather,
 			 QUERY(String, city)) {
-      if (!city) {
+      if (!city || city->empty()) {
         return createResponse(Status::CODE_400, "City not found");
       }
 
+      try {
+          WeatherClient client;
+          std::string cityStr = city;
+
+          auto [weather, air] = client.getWeather(cityStr);
+
+          nlohmann::json result = {
+              { "location", weather.location },
+              { "country", weather.country },
+              { "tempC", weather.tempC },
+              { "tempF", weather.tempF },
+              { "condition", weather.condition },
+              { "windKph", weather.windKph },
+              { "windMph", weather.windMph },
+              { "humidity", weather.humidity },
+              { "epaIndex", air.epaIndex },
+              { "epaCategory", air.epaCategory }
+          };
+
+          return createResponse(Status::CODE_200, result.dump(2));
+
+      } catch (const std::exception& e) {
+            return createResponse(Status::CODE_500, e.what());
+      }
+
       // temporary response
-      oatpp::String message = "Weather for " + city;
-      return createResponse(Status::CODE_200, message);
+      // oatpp::String message = "Weather for " + city;
+      // return createResponse(Status::CODE_200, message);
     }
 };
 
